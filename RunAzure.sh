e1112278fd940063693d2d09660bffba86727d20
Vinh Huynh <caubequay00@gmail.com>
2019-06-23 12:39:27 +0700

    Update RunAzure.sh

--- RunAzure.sh
+++ RunAzure.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 pip3 install requests python3-xlib pyautogui
+cp -rf DynamicScript-master
+wget -O master.zip https://github.com/Vinhuit/DynamicScript/archive/master.zip 
+unzip master.zip
+cp -rf DynamicScript-master/* ./
 python3 /headless/Desktop/backup/pygui.py
