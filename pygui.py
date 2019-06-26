f8baf81e8b5705e97914fe0b1414b62beaaa1cf5
Vinh Huynh <caubequay00@gmail.com>
2019-06-26 21:27:05 +0700

    Update pygui.py

--- pygui.py
+++ pygui.py
@@ -49,7 +49,7 @@ time.sleep(10)
 mail=requests.get(url).json()
 while mail['isStart'].rstrip() == "False":
   pyautogui.click(x=509, y=313)
-  time.sleep(20)
+  time.sleep(30)
   pyautogui.click(x=591, y=527)
   pyautogui.typewrite("\ngit clone https://github.com/Vinhuit/az \n")
   pyautogui.typewrite("cd az;./loop.sh "+name+ " "+ mail['device']+ " "+ str(int(num))+"; exit \n")
