6235d5f5cc18f7f2d8fd76f356fe853179b6c3d3
Vinh Huynh <caubequay00@gmail.com>
2019-07-17 12:48:09 +0700

    Update RunAzure.sh

--- RunAzure.sh
+++ RunAzure.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#pip3 install --upgrade pip
-pip3 install requests python3-xlib pyautogui
-python3 -m pip install requests python3-xlib pyautogui --user
+pip3 install --upgrade pip
+pip3 install requests python3-xlib pyautogui===0.9.39
+python3 -m pip install requests python3-xlib pyautogui===0.9.39 --user
 python3 /headless/Desktop/backup/pygui.py
