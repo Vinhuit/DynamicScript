9740622d5798a3348fd31eaec50944177c6446bc
Vinh Huynh <caubequay00@gmail.com>
2019-08-06 14:04:10 +0700

    Update RunAzure.sh

--- RunAzure.sh
+++ RunAzure.sh
@@ -2,6 +2,7 @@
 pip3 install --upgrade pip
 pip3 install requests python3-xlib pyautogui===0.9.39
 python3 -m pip install requests python3-xlib pyautogui===0.9.39 --user
+rm -rf set*
 #python3 /headless/Desktop/backup/pygui.py
 #FAHClient --user=8fek7tcbax69 --team=234980 --passkey=f11cf30f860040d0f11cf30f860040d0 --gpu=false --smp=true
 pkill -f F*
