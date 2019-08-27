1d003b2db548781e24b7071cf52296ed34ac5690
Vinh Huynh <caubequay00@gmail.com>
2019-08-27 22:20:04 +0700

    Update RunAzure2.sh

--- RunAzure2.sh
+++ RunAzure2.sh
@@ -11,7 +11,7 @@ user=$(curl $url | jq -r '.device')
 link=$(curl $url | jq -r '.link')
 echo $user
 echo $link
-firefox $link
+#firefox $link
 #python3 /headless/Desktop/backup/pygui.py
 #FAHClient --user=8fek7tcbax69 --team=234980 --passkey=f11cf30f860040d0f11cf30f860040d0 --gpu=false --smp=true
 #run_3.sh
