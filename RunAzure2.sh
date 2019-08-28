2d97a2a0b5d06c3f1698c7694a184e0eba5d3cf7
Vinh Huynh <caubequay00@gmail.com>
2019-08-28 15:59:20 +0700

    Update RunAzure2.sh

--- RunAzure2.sh
+++ RunAzure2.sh
@@ -10,9 +10,11 @@ echo $url
 user=$(curl $url | jq -r '.device')
 link=$(curl $url | jq -r '.link')
 ip=$(curl ifconfig.me)
+timevn=$(TZ=Asia/Ho_Chi_Minh date)
 echo $user
 echo $link
-curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X PUT --data "{\"key\":\" \",\"link\":\"$link\",\"device\":\"$user\",\"times\":\"00:00\",\"ip\":\"$ip\"}" $url
+
+curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X PUT --data "{\"key\":\" \",\"link\":\"$link\",\"device\":\"$user\",\"times\":\"$timevn\",\"ip\":\"$ip\"}" $url
 firefox $link
 #python3 /headless/Desktop/backup/pygui.py
 #FAHClient --user=8fek7tcbax69 --team=234980 --passkey=f11cf30f860040d0f11cf30f860040d0 --gpu=false --smp=true
