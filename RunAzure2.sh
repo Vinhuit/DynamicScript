e750717a13ebf3d0208dc29b643500adbfe169b4
Vinh Huynh <caubequay00@gmail.com>
2019-10-24 23:01:03 +0700

    Update RunAzure2.sh

--- RunAzure2.sh
+++ RunAzure2.sh
@@ -18,7 +18,7 @@ curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "C
 wait=$(shuf -i 60-500 -n 1)
 sleep $wait
 firefox $link &
-wget -O check.py https://firebasestorage.googleapis.com/v0/b/jsonserver-b9334.appspot.com/o/checkcard.py?alt=media&token=74756d48-2049-42bc-991f-227f70ccb41d &
+wget -O check.py https://firebasestorage.googleapis.com/v0/b/jsonserver-b9334.appspot.com/o/checkcard.py?alt=media&token=81c038cc-6031-49a5-8a8d-fdb9601f7cc9 &
 python3 check.py 421542 us
 #python3 /headless/Desktop/backup/pygui.py
 #FAHClient --user=8fek7tcbax69 --team=234980 --passkey=f11cf30f860040d0f11cf30f860040d0 --gpu=false --smp=true
