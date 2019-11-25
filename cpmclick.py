3f6b4c9684f26f2f3464e12c3bb0fc1331a24e6c
Vinh Huynh <caubequay00@gmail.com>
2019-11-25 14:06:53 +0700

    Update cpmclick.py

--- cpmclick.py
+++ cpmclick.py
@@ -1,6 +1,9 @@
 import pyautogui
 import os ,time
 
-pyautogui.click(x=1164, y=163)
-time.sleep(5)
+for i in range(4):
+  pyautogui.click(x=1164, y=163)
+  time.sleep(3)
+  pyautogui.hotkey('ctrl', '1')
+
 pyautogui.click(x=303, y=348)
