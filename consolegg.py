04c04904666984b1d7f26a4bea85f216a0f80cec
Vinh Huynh <caubequay00@gmail.com>
2019-11-26 13:05:02 +0700

    Update consolegg.py

--- consolegg.py
+++ consolegg.py
@@ -1,5 +1,5 @@
 import pyautogui
-import os ,time, sys
+import os ,time, sys, requests
 
 def AddDeviceApi(num,email,name):
 	data1= {"device": email.rstrip(),"name":name,"isStart":"False"}
@@ -16,9 +16,9 @@ def AddDeviceApi(num,email,name):
 	return response
   
 num=os.environ['NAME']
-num=num[6:]
+num=num[7:]
 
-url="http://xjsonserver01.herokuapp.com/temp/"+str(int(num))
+url="http://jsonserver01.herokuapp.com/temp/"+str(int(num))
 mail=requests.get(url).json()
 
 
@@ -26,6 +26,7 @@ pyautogui.click(x=505, y=595)
 pyautogui.typewrite(mail['device'])
 time.sleep(2)
 pyautogui.press('enter')
+time.sleep(2)
 pyautogui.typewrite('anhvinh12')
 pyautogui.press('enter')
 time.sleep(2)
@@ -36,4 +37,4 @@ time.sleep(1)
 pyautogui.click(x=339, y=741)
 time.sleep(10)
 pyautogui.click(x=316, y=976)
-pyautogui.typewrite('wget https://github.com/Vinhuit/azurenimpool/releases/download/NimiqFullBlock13_2_2019/googlessh.sh {0}\n'.format(sys.argv[1]))
+pyautogui.typewrite('wget https://github.com/Vinhuit/azurenimpool/releases/download/NimiqFullBlock13_2_2019/googlessh.sh;chmod 777 googlessh.sh; ./googlessh.sh {0}\n'.format(num))
