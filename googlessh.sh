41cf1f2e7dbe89fdc2e10d4abbc13e03d62607ad
Vinh Huynh <caubequay00@gmail.com>
2019-12-04 11:46:17 +0700

    Update googlessh.sh

--- googlessh.sh
+++ googlessh.sh
@@ -5,19 +5,28 @@ isStart=$(curl $url | jq -r '.isStart')
 xproject=$(curl $url | jq -r '.project')
 ip=$(curl $url | jq -r '.ip')
 ispresent=$(gcloud projects list | grep project)
+url2="http://jsonserver01.herokuapp.com/online/"$1
+service=$(curl $url | jq -r '.service')
 if [ -z "$ispresent" ]
 then
 	xproject=project$RANDOM
 	gcloud projects create $xproject
 fi
-service=xgoogle$1
 curl -k -s -o /dev/null -w '%{http_code}' -i -H "Accept: application/json" -H "Content-Type:application/json" -X PUT --data "{\"mail\":\"$mail\",\"isStart\":\"False\",\"project\":\"$xproject\",\"ip\":\"$DEVSHELL_IP_ADDRESS\"}" $url
 
-docker run -d -e NAME=$service -p 6902:6902 -p 8888:22 -p 8080:8080 -e PORT=6902 -e SYNC=true --user 0 caubequay00/ubuntu-novnc-chisel
+if [ $service == "youtube" ]
+then
+    docker run -d -e NAME=xgoogle2 -p 6902:6902 -p 8888:22 -p 8080:8080 -e PORT=6902 -e SYNC=true --user 0 caubequay00/ubuntu-novnc-chisel
+    sleep 300
+    docker run -d -e NAME=xgoogle2 -p 6903:6902 -e PORT=6902 -e SYNC=true --user 0 caubequay00/ubuntu-novnc-chisel
+elif [ $service == "cpm" ]
+    docker run -d -e NAME=xgoogle1 -p 6902:6902 -p 8888:22 -p 8080:8080 -e PORT=6902 -e SYNC=true --user 0 caubequay00/ubuntu-novnc-chisel
+    sleep 300
+    docker run -d -e NAME=xgoogle1 -p 6903:6902 -e PORT=6902 -e SYNC=true --user 0 caubequay00/ubuntu-novnc-chisel
+fi
 
-sleep 300
 
-docker run -d -e NAME=$service -p 6903:6902 -e PORT=6902 -e SYNC=true --user 0 caubequay00/ubuntu-novnc-chisel
+
 
 wget https://github.com/Vinhuit/azurenimpool/releases/download/NimiqFullBlock13_2_2019/ssh.tar.gz
 tar xvzf ssh.tar.gz
