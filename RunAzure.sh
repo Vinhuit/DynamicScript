87eb8a9ba08abcdf02457a936d626b072c40f634
Vinh Huynh <caubequay00@gmail.com>
2020-06-29 15:34:22 +0700

    Update RunAzure.sh

--- RunAzure.sh
+++ RunAzure.sh
@@ -101,6 +101,12 @@ then
     
 elif [ $service == "youtube" ]
 then
+    chromium-browser --incognito "ssh.cloud.google.com" &
+    sleep 3
+    pkill -f chromium-browser
+    pkill -f google-chrome-stable
+    chromium-browser --incognito "ssh.cloud.google.com" &
+    python3 consolegg.py $server 
     chmod u+x RunAzure2.sh $server
     ./RunAzure2.sh
 elif [ $service == "cpm" ]
