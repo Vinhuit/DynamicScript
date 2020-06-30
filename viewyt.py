cdaf86acff08c7d6cc06e1c9b63f19af6c8839e5
Vinh Huynh <caubequay00@gmail.com>
2020-06-30 23:19:15 +0700

    Update viewyt.py

--- viewyt.py
+++ viewyt.py
@@ -173,25 +173,24 @@ def bot(id):
 						except:pass
 def logingmail(driver,email):
 	driver.get("https://accounts.google.com/signin/v2/identifier?service=youtube&uilel=3&passive=true&continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Den%26next%3Dhttps%253A%252F%252Fwww.youtube.com%252F&hl=en&ec=65620&flowName=GlifWebSignIn&flowEntry=ServiceLogin")
-
-	button = WebDriverWait(driver, 10).until(
-		EC.element_to_be_clickable((By.ID, "identifierId"))
-		)
-	button.send_keys(email)
-	button = WebDriverWait(driver, 10).until(
-		EC.element_to_be_clickable((By.ID, "identifierNext"))
-		)
-	button.click()
-	button = WebDriverWait(driver, 10).until(
-		EC.element_to_be_clickable((By.NAME, "password"))
-		)
-	button.send_keys("anhvinh12")
-		#confirmation-next\
-	button = WebDriverWait(driver, 10).until(
-		EC.element_to_be_clickable((By.ID, "passwordNext"))
-		)
-	button.click()
 	try:
+		button = WebDriverWait(driver, 10).until(
+			EC.element_to_be_clickable((By.ID, "identifierId"))
+			)
+		button.send_keys(email)
+		button = WebDriverWait(driver, 10).until(
+			EC.element_to_be_clickable((By.ID, "identifierNext"))
+			)
+		button.click()
+		button = WebDriverWait(driver, 10).until(
+			EC.element_to_be_clickable((By.NAME, "password"))
+			)
+		button.send_keys("anhvinh12")
+			#confirmation-next\
+		button = WebDriverWait(driver, 10).until(
+			EC.element_to_be_clickable((By.ID, "passwordNext"))
+			)
+		button.click()
 					
 		button = WebDriverWait(driver, 10).until(
 			EC.element_to_be_clickable((By.ID, 'accept'))
