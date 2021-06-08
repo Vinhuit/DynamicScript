67577df46756b1b67410a0c1bc8a6ff3b1263acc
Vinh Huynh <caubequay00@gmail.com>
2021-06-09 00:20:29 +0700

    Create install_ngrok.sh

---
+++ install_ngrok.sh
@@ -0,0 +1,4 @@
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+tar xvf ngrok-stable-linux-amd64.zip
+./ngrok authtoken 1s4gEqxeYySI8vuT5CaQv43IU1i_5MQZGYmcDNfYR7mdVnr3P
+./ngrok tcp 22
