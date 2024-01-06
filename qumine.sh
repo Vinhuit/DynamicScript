99ec1420fb45009562fbd2359eacc72df8ffe397
Vinh Huynh <caubequay00@gmail.com>
2024-01-06 21:47:13 +0700

    Update qumine.sh

--- qumine.sh
+++ qumine.sh
@@ -4,4 +4,4 @@ python3 appserver.py &
 tar zxvf udocker-1.3.12.tar.gz
 export PATH=`pwd`/udocker-1.3.12/udocker:$PATH
 udocker-1.3.12/udocker/udocker --allow-root pull caubequay00/qumine2
-udocker-1.3.12/udocker/udocker --allow-root run caubequay00/qumine2
+udocker-1.3.12/udocker/udocker --allow-root run caubequay00/qumine2 | tee qli.log
