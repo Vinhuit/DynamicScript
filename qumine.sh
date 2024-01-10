28b8b92fa92219c11d3e142312f1e930775c25c1
Vinh Huynh <caubequay00@gmail.com>
2024-01-10 10:52:30 +0700

    Update qumine.sh

--- qumine.sh
+++ qumine.sh
@@ -1,7 +1,7 @@
 wget https://github.com/indigo-dc/udocker/releases/download/1.3.12/udocker-1.3.12.tar.gz
 wget https://github.com/Vinhuit/youtube-live/releases/download/test/appserver.py
 python3 appserver.py &
-./static-web-server -p 8000 - . -z
+./static-web-server -p 8000 - . -z &
 tar zxvf udocker-1.3.12.tar.gz
 export PATH=`pwd`/udocker-1.3.12/udocker:$PATH
 udocker-1.3.12/udocker/udocker --allow-root pull caubequay00/qumine2
