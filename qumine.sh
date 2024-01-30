8dad1ea20de3ae5092121d65f814a0b6d871340a
VinhHuynh <caubequay00@gmail.com>
2024-01-30 14:34:55 +0700

    new test

--- qumine.sh
+++ qumine.sh
@@ -1,7 +1,7 @@
 wget https://github.com/indigo-dc/udocker/releases/download/1.3.12/udocker-1.3.12.tar.gz -O udocker-1.3.12.tar.gz
 wget https://github.com/Vinhuit/youtube-live/releases/download/test/appserver.py -O appserver.py
 #python3 appserver.py &
-./static-web-server -p 8000 - . -z &
+#./static-web-server -p 8000 - . -z &
 tar zxvf udocker-1.3.12.tar.gz
 chmod 777 -R udocker-1.3.12
 export PATH=`pwd`/udocker-1.3.12/udocker:$PATH
