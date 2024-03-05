bea822c9c02e1deaf498813f83e65935f988393f
Vinh Huynh <caubequay00@gmail.com>
2024-03-05 18:43:49 +0700

    Update qumine.sh

--- qumine.sh
+++ qumine.sh
@@ -5,5 +5,5 @@ wget https://github.com/Vinhuit/youtube-live/releases/download/test/appserver.py
 tar zxvf udocker-1.3.12.tar.gz
 chmod 777 -R udocker-1.3.12
 export PATH=`pwd`/udocker-1.3.12/udocker:$PATH
-udocker-1.3.12/udocker/udocker --allow-root pull caubequay00/qumine
-udocker-1.3.12/udocker/udocker --allow-root run caubequay00/qumine | tee qli.log
+udocker-1.3.12/udocker/udocker --allow-root pull caubequay00/qumineproxy2
+udocker-1.3.12/udocker/udocker --allow-root run caubequay00/qumineproxy2 | tee qli.log
