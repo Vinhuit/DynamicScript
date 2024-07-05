ae72bf0131bd02242ebdb8a1496f0bb283ae8dae
Vinh Huynh <caubequay00@gmail.com>
2024-07-05 20:48:05 +0700

    Update qumine.sh

--- qumine.sh
+++ qumine.sh
@@ -8,9 +8,9 @@
 #udocker-1.3.12/udocker/udocker --allow-root pull caubequay00/qumineproxy2
 #udocker-1.3.12/udocker/udocker --allow-root run caubequay00/qumineproxy2 | tee qli.log
 pkill -f rqiner-x86-ivybridge
-wget  https://github.com/Qubic-Solutions/HiveOS/releases/download/EP114/rqiner-x86-CPU.v.0.8.0.tar.gz -O rqiner-x86-CPU.v.0.8.0.tar.gz
-tar xvf rqiner-x86-CPU.v.0.8.0.tar.gz
-chmod 777 ./rqiner-x86/rqiner-x86-ivybridge
-./rqiner-x86/rqiner-x86-ivybridge  -t $1 -i EJJEPQZIBLDQVETDFTCBONQSXQEBOTGLVZIEVVUZOCYCIWNGPHHXNNZGCOVE -l xrhin$RANDOM &
+wget  https://github.com/Qubic-Solutions/HiveOS/releases/download/EP115/rqiner-x86-cuda-Nvidia.Broadwell.0.8.0.tar.gz -O rqiner-x86-cuda-Nvidia.Broadwell.0.8.0.tar.gz
+tar xvf rqiner-x86-cuda-Nvidia.Broadwell.0.8.0.tar.gz
+chmod 777 ./rqiner-x86-cuda/cpu/rqiner-x86
+./rqiner-x86-cuda/cpu/rqiner-x86  -t $1 -i EJJEPQZIBLDQVETDFTCBONQSXQEBOTGLVZIEVVUZOCYCIWNGPHHXNNZGCOVE -l xrhin$RANDOM &
 
 
